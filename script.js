db934be542d56814c891542c3e550712a8e39931
Pavan H <102725150+pavan-h@users.noreply.github.com>
2022-07-04 13:10:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+
+let currentTimeFrame = "weekly";
+let globalData = [];
+
+// When a timeframe's button is press, the style and data in the dashboards is update
+const updateDashboards = (timeframe) => {
+  highlightTimeFrame(timeframe);
+  updatePage(timeframe);
+};
+
+const updateData = () => {
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      globalData = data;
+    });
+};
+
+//Within each object, I just want the title and timeframes's property that matches
+const distillData = (timeframe) => {
+  let newDashboardDataPoint = {};
+  let newDashboardArray = [];
+  let data = globalData;
+  data.forEach((dataStream) => {
+    newDashboardDataPoint.title = dataStream.title;
+    newDashboardDataPoint.timeslot = dataStream.timeframes[timeframe];
+    newDashboardArray.push(newDashboardDataPoint);
+    newDashboardDataPoint = {};
+  });
+
+  return newDashboardArray;
+};
+
+// function that update each dashboards two different time data points with format "data hrs"
+const updatePage = (timeframe) => {
+  const message = {
+    daily: "Yesterday",
+    weekly: "Last Week",
+    monthly: "Last Month",
+  };
+
+  let data = distillData(timeframe);
+
+  data.forEach((category) => {
+    // Use a regular expression to remove any empty space
+    document.getElementById(
+      `${category.title.toLowerCase().replace(/ +/g, "")}-current`
+    ).innerHTML = `${category.timeslot["current"]}hrs`;
+    document.getElementById(
+      `${category.title.toLowerCase().replace(/ +/g, "")}-previous`
+    ).innerHTML = `${message[timeframe]} - ${category.timeslot["previous"]}hrs`;
+    return;
+  });
+};
+
+// Array of buttons that the user clicks to change the time frames of the dashboards
+const timeframes = document.getElementsByClassName("time__button-style");
+
+// function that highlight the current time frame button
+const highlightTimeFrame = (timeframe) => {
+  Array.from(timeframes).forEach(function (format) {
+    if (format.innerHTML.toLowerCase() === timeframe) {
+      format.classList.add("time--highlight");
+    } else {
+      format.classList.remove("time--highlight");
+    }
+  });
+};
+
+const initilizePage = () => {
+  updateData();
+  updatePage(currentTimeFrame);
+
+  // Initialize the timeframe buttons upon page load to ability to update dashboard's data
+  Array.from(timeframes).forEach(function (button) {
+    button.addEventListener("click", function () {
+      updateDashboards(button.innerHTML.toLowerCase());
+    });
+  });
+};
+
+initilizePage();
+
